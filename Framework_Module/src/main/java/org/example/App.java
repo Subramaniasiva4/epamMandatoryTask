4afbaeb4d3a9acb3f61309b1d2aa3286350fffed
Subramaniasiva_Sg <Subramaniasiva_Sg@epam.com>
2022-08-08 16:46:11 +0530

    done

--- Framework_Module/src/main/java/org/example/App.java
+++ Framework_Module/src/main/java/org/example/App.java
@@ -1,20 +1,12 @@
 package org.example;
 
-
 import org.apache.log4j.Logger;
 
 /**
- * Hello world!
+ * Logger
  *
  */
 public class App
 {
-
     static Logger logger=  Logger.getLogger(App.class.getName());
-    /*public static void main( String[] args )
-
-    {
-        logger.info("dadsa");
-        System.out.println( "Hello World!" );
-    }*/
 }
